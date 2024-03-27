692b0d791c9cc345e5882ec7e07534c90b51d7f0
dilolo70 <77835632+dilolo70@users.noreply.github.com>
2024-03-27 03:13:45 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
     Bienvenu sur le site de ASPE
+    <div> 
+      Notre site est en production actuellement
+    </div>
     </main>
   );
 }
