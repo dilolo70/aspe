e78e8e99c245ed0d3412e81fdb9010b6aaa4da14
dilolo70 <77835632+dilolo70@users.noreply.github.com>
2024-03-29 16:44:00 +0100

    hero section add

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
-import Image from "next/image";
+import Card from "@/components/Section/card";
+import Hero from "@/components/Section/hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    Bienvenu sur le site de ASPE
-    <div> 
-      Notre site est en production actuellement
+    <div className="">
+  
+     
+  <Hero/>
     </div>
-    </main>
   );
 }
---
+++ components/Section/hero.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { motion } from "framer-motion";
+import React from "react";
+import { ImagesSlider } from "../ui/images";
+
+
+
+const hero = () => {
+    const images = [
+        "/image/hero1.jpg",
+        "/image/hero2.jpg",
+        "/image/hero3.jpg",
+        "/image/hero4.jpg",
+    ];
+    return (
+        <ImagesSlider className="h-[40rem]" images={images}>
+            <motion.div
+                initial={{
+                    opacity: 0,
+                    y: -80,
+                }}
+                animate={{
+                    opacity: 1,
+                    y: 0,
+                }}
+                transition={{
+                    duration: 0.6,
+                }}
+                className="z-50 flex flex-col justify-center items-center"
+            >
+                <motion.p className="font-bold text-3xl md:text-6xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4">
+                    Cultiver l{"'"} excellence en chacun  <br /> Vers un avenir meilleur
+                </motion.p>
+                <div>
+                    <button className="px-4 py-2 backdrop-blur-sm border bg-blue-300/10 border-blue-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+                        <span>Rejoignez Nous Maintenant →</span>
+                        <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-blue-500 to-transparent" />
+                    </button>
+                </div>
+            </motion.div>
+        </ImagesSlider>
+    );
+}
+
+export default hero
